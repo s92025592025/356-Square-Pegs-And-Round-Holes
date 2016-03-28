5e0de11b56c3fe52cba28e57f69972db870d8fc2
s92025592025 <s92025592025@gmail.com>
2016-03-28 15:10:36 -0700

    Passed Udebug, but got WA

--- Main.java
+++ Main.java
@@ -1,21 +1,39 @@
 import java.util.Scanner;
 
 class Main{
-	private final static int IN = 2314;
-	private final static int UNSURE = 3215;
-	private final static int OUT = -4563;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(sc.hasNextInt()){
 			int n = sc.nextInt();
-			int points[][] = new int[(n * 2 + 1)][(n * 2 + 1)];
+			boolean checked[][] = new boolean[2 * n][2 * n];
+			int count[] = {0, 0};
+			count(checked, 0, 0, n, count);
+			System.out.println("In the case n = " + n + ", " + count[1] + " cells contain segments of the circle.");
+			System.out.println("There are " + count[0] + " cells completely contained in the circle.");
+			System.out.println();
 		}
 	}
 
-	public static void initinalizeArray(int[][] array){
-		for(int i = 0; i < array.length; i++){
-			Arrays.fill(array[i], UNSURE);
+	public static void count(boolean checked[][], int x, int y, int n, int count[]){
+		if(x < 2 * n && y < 2 * n && !checked[x][y]){
+			checked[x][y] = true;
+			if(inClrcle(x, y, n) && inClrcle(x + 1, y, n) && inClrcle(x, y + 1, n) && inClrcle(x + 1,y + 1, n)){
+				count[0]++;
+				count(checked, x + 1, y, n, count);
+				count(checked, x, y + 1, n, count);
+			}else if(inClrcle(x, y, n) || inClrcle(x + 1, y, n) || inClrcle(x, y + 1, n) || inClrcle(x + 1,y + 1, n)){
+				count[1]++;
+				count(checked, x + 1, y, n, count);
+				count(checked, x, y + 1, n, count);
+			}else{
+				count(checked, x + 1, y, n, count);
+				count(checked, x, y + 1, n, count);
+			}
 		}
+	}
+
+	public static boolean inClrcle(int x, int y, int n){
+		return (x - n) * (x - n) + (y - n) * (y - n) <= (2 * n - 1) * (2 * n - 1) / 4 ;
 	}
 }
